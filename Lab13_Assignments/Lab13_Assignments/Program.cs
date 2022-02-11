1c61ce0d6ad8cc390a5bca703805d2e7082613c7
Bogdan748 <andriesbogdan00@gmail.com>
2022-02-11 20:06:06 +0200

    Min with recurcion

---
+++ Lab13_Assignments/Lab13_Assignments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab13_Assignments
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = VectorHelper.GetVector();
+
+            Console.WriteLine();
+            Console.WriteLine($"Min valuea is: {VectorHelper.RecursiveMin(array)}");
+            Console.WriteLine($"To find the value I made {VectorHelper.RecursiveSteps} steps");
+
+
+        }
+    }
+}
---
+++ Lab13_Assignments/Lab13_Assignments/VectorHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab13_Assignments
+{
+    public static class VectorHelper
+    {
+        public static int RecursiveSteps = 0;
+        public static  int[] GetVector()
+        {
+            Console.WriteLine("Type values separeted by commas (Ex: '1,2,3')");
+            string input = Console.ReadLine();
+
+            int[] array = input.Split(',',StringSplitOptions.RemoveEmptyEntries).Select(Int32.Parse).ToArray();
+
+            return array;
+        }
+
+        public static int RecursiveMin(int[] array)
+        {
+            return RecursiveMin(array, 0);
+        }
+
+        private static int RecursiveMin(int[] array,int n)
+        {
+            RecursiveSteps += 1;
+
+            if (array.Length - 1 == n) return array[n];
+
+            return Math.Min(array[n], RecursiveMin(array, n + 1));
+        }
+    }
+}
